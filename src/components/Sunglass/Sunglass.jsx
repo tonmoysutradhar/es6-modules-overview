4d8d06841b25486c7376df4d8ecf119006b044a0
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-11-13 20:18:57 +0600

    ES6 Modules Overview

---
+++ src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,12 @@
+
+import './Sunglass.css';
+import Watch from '../Watch/Watch';
+const Sunglass = () => {
+    return (
+        <div>
+            <Watch></Watch>
+        </div>
+    );
+};
+
+export default Sunglass;
---
+++ src/components/Watch/Watch.jsx
@@ -0,0 +1,11 @@
+
+
+const Watch = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Watch;
